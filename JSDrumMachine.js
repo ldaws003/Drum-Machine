6519267235dae3e0fdaa099570a8c4f8f8924a9c
Leon Dawson <41959451+ldaws003@users.noreply.github.com>
2018-11-06 12:30:59 -0500

    Add files via upload

---
+++ JSDrumMachine.js
@@ -0,0 +1,41 @@
+
+$(document).keydown(function(e){
+    switch(e.key){
+      case 'q':
+      case 'Q':
+        $('#Q').trigger('click');
+        break;
+      case 'w':
+      case 'W':
+        $('#W').trigger('click');
+        break;
+      case 'e':
+      case 'E':
+        $('#E').trigger('click');
+        break;
+      case 'a':
+      case 'A':
+        $('#A').trigger('click');
+        break;
+      case 's':
+      case 'S':
+        $('#S').trigger('click');
+        break;
+      case 'd':
+      case 'D':
+        $('#D').trigger('click');
+        break;
+      case 'z':
+      case 'Z':
+        $('#Z').trigger('click');
+        break;
+      case 'x':
+      case 'X':
+        $('#X').trigger('click');
+        break;
+      case 'c':
+      case 'C':
+        $('#C').trigger('click');
+        break;
+    }
+});
